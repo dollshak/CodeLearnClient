aff3f3cc480a950dcd065dc543e031decb8f2b55
dollshak <dollshak@post.bgu.ac.il>
2022-12-02 15:57:52 +0200

    http

--- src/pages/CodeBlockPage/CodeBlockPage.jsx
+++ src/pages/CodeBlockPage/CodeBlockPage.jsx
@@ -6,7 +6,7 @@ import io from "socket.io-client";
 import configData from "../../config.json";
 
 const socket = io.connect(
-  "http://ec2-3-86-186-64.compute-1.amazonaws.com/mysocket"
+  "http://ec2-3-86-186-64.compute-1.amazonaws.com:80/mysocket"
 );
 // const socket = io.connect(
 //   configData.production
